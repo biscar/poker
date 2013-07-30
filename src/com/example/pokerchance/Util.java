682597f02f0245dc1ab36c424611aa84be78274a
Vital Stukov <stukov_89@mail.ru>
2013-07-30 19:58:14 +0300

    my

---
+++ src/com/example/pokerchance/Util.java
@@ -0,0 +1,24 @@
+package com.example.pokerchance;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
+public class Util {
+	
+ public static String formatChance(float num) {
+	 double value = new BigDecimal(num).setScale(3, RoundingMode.UP).doubleValue();
+	 return String.format("%2.2f : 1", value); 
+ }
+	
+ public static String formatUnit(float num) {
+	 
+	return String.format("%2.0f", num); 
+ }
+	
+ public static String formatPercent(float num) {
+	 double value = new BigDecimal(num).setScale(3, RoundingMode.UP).doubleValue();
+	 return String.format("%2.2f", value) + "%"; 
+ }
+
+}
